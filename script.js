199e1aeb2f8c3e3eefa867f848ee888f305e9480
irum-shah78 <irams4812@gmail.com>
2024-04-26 20:18:24 +0500

    Dashboard navbar

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const body = document.querySelector("body"),
   modeToggle = body.querySelector(".toggle");
+sideBar = body.querySelector("nav");
+sidebarToggle = body.querySelector(".sidebar-toggle");
 
 modeToggle.addEventListener('click', () => {
-body.classList.toggle('dark');
+  body.classList.toggle('dark');
 });
+
+sidebarToggle.addEventListener('click', () => {
+  sideBar.classList.toggle('close');
+})
