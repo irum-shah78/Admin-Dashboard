9a0d08a20ad8b6d480a0bd5a42ed82ae4f9c3f72
irum-shah78 <irams4812@gmail.com>
2024-04-27 21:42:55 +0500

    completed

--- script.js
+++ script.js
@@ -3,10 +3,30 @@ const body = document.querySelector("body"),
 sideBar = body.querySelector("nav");
 sidebarToggle = body.querySelector(".sidebar-toggle");
 
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.toggle('dark');
+}
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+  sideBar.classList.toggle('close');
+}
+
 modeToggle.addEventListener('click', () => {
   body.classList.toggle('dark');
+  if (body.classList.contains('dark')) {
+    localStorage.setItem("mode", "dark");
+  } else {
+    localStorage.setItem("mode", "light");
+  }
 });
 
 sidebarToggle.addEventListener('click', () => {
   sideBar.classList.toggle('close');
+  if (sideBar.classList.contains('close')) {
+    localStorage.setItem("status", "close");
+  } else {
+    localStorage.setItem("status", "open");
+  }
 })
